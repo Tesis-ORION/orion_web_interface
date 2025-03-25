5d15c2c48ee15524add60949c19007fe1a5253d3
miguelgonrod <miguelgonrod2004@gmail.com>
2025-03-25 11:38:19 -0500

    Feat: Added dockerfile to deploy web app, and lidar support

--- orion_web_interface/interfaz-orion/src/pages/Home.jsx
+++ orion_web_interface/interfaz-orion/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import SpeechComponent from "../components/SpeechComponent";
 import ImageComponent from "../components/ImageComponent";
 import TwistSubComponent from "../components/TwistSubComponent";
 import JoystickComponent from "../components/JoystickComponent";
+import LidarComponent from "../components/LidarComponent";
 
 const Home = () => {
     return (
       <div>
         <SpeechComponent />
+        <LidarComponent rosbridgeUrl="ws://localhost:9090" topicName="/scan"/>
         <div className="image-container">
           <ImageComponent title="Imagen a color" topic="/apc/left/image_base64" />
           <ImageComponent title="Imagen de Profundidad" topic="/apc/depth/image_base64" />
