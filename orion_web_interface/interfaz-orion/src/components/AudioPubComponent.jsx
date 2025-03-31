2f66eeeae4e448f62d25f774c00e7ada1116b869
alexoberco <alejandro.bermudez.fajardo@gmail.com>
2025-03-31 12:21:36 -0500

    fix: .env for test

--- orion_web_interface/interfaz-orion/src/components/AudioPubComponent.jsx
+++ orion_web_interface/interfaz-orion/src/components/AudioPubComponent.jsx
@@ -33,8 +33,9 @@ const AudioPubComponent = () => {
         audioChunksRef.current = [];
         const formData = new FormData();
         formData.append('file', audioBlob, 'grabacion.webm');
+
         try {
-          const response = await fetch('http://localhost:5000/upload', {
+          const response = await fetch(`${import.meta.env.VITE_ORION_STT}`, {
             method: 'POST',
             body: formData,
           });
